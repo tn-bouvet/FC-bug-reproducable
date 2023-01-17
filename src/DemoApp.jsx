5eb4e2f8f367de9fbc6cfe5167668d46982b11f4
tn-bouvet <88835417+tn-bouvet@users.noreply.github.com>
2023-01-17 13:02:41 +0100

    2 files modified, 2 deleted

--- src/DemoApp.jsx
+++ src/DemoApp.jsx
@@ -2,29 +2,43 @@ import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
 
-export default class DemoApp extends React.Component {
-  render() {
-    return (
-      <div className="demo-app">
-        <div className="demo-app-main">
-          <FullCalendar
-            plugins={[resourceTimelinePlugin]}
-            initialView="resourceTimeline"
-            // Removing the following line fixes it
-            resourceAreaHeaderContent="Resource Area Header"
-            resources={[
-              { num: '22', name: 'John' },
-              { num: '66', name: 'Glen' },
-              { num: '32', name: 'Joan' },
-              { num: '55', name: 'Matt' },
-            ]}
-            resourceAreaColumns={[
-              { field: 'num', headerContent: () => 'Number' },
-              { field: 'name', headerContent: 'Name' },
-            ]}
-          />
-        </div>
+export const DemoApp = () => {
+  return (
+    <div className="demo-app">
+      <div className="demo-app-main">
+        {/* Column headers broken, but resource title shown */}
+        <FullCalendar
+          plugins={[resourceTimelinePlugin]}
+          initialView="resourceTimeline"
+          // Removing the following line fixes it
+          resourceAreaHeaderContent="Resource Area Header"
+          resources={[
+            { num: '22', name: 'John' },
+            { num: '66', name: 'Glen' },
+            { num: '32', name: 'Joan' },
+            { num: '55', name: 'Matt' },
+          ]}
+          resourceAreaColumns={[
+            { field: 'num', headerContent: () => 'Number' },
+            { field: 'name', headerContent: 'Name' },
+          ]}
+        />
+        {/* Column headers work, but no resource title */}
+        <FullCalendar
+          plugins={[resourceTimelinePlugin]}
+          initialView="resourceTimeline"
+          resources={[
+            { num: '22', name: 'John' },
+            { num: '66', name: 'Glen' },
+            { num: '32', name: 'Joan' },
+            { num: '55', name: 'Matt' },
+          ]}
+          resourceAreaColumns={[
+            { field: 'num', headerContent: () => 'Number' },
+            { field: 'name', headerContent: 'Name' },
+          ]}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import DemoApp from './DemoApp'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { DemoApp } from './DemoApp';
 
-document.addEventListener('DOMContentLoaded', function() {
-  createRoot(document.body.appendChild(document.createElement('div')))
-    .render(<DemoApp />)
-})
+document.addEventListener('DOMContentLoaded', function () {
+  createRoot(document.body.appendChild(document.createElement('div'))).render(
+    <DemoApp />
+  );
+});
